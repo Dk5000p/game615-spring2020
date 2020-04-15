1cb65a95ab9a9ef234c4fa7cb27859c15a1917ce
Doug Kent <dk5000p@gmail.com>
2020-04-14 23:39:19 -0400

    Changes

---
+++ exercises/ARChess-master (3)/ARChess-master/Assets/_Scripts/ColorSelectionGameStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColorSelectionGameStart : MonoBehaviour
+{
+ public int scene=1;
+ public void chooseBlack()
+    {
+        GameState.playerColor = "Black";
+        AI.ai_player = "White";
+        InputController.playerColor = "Black";
+    }
+ public void chooseWhite()
+    {
+        GameState.playerColor = "White";
+        AI.ai_player = "Black";
+        InputController.playerColor = "White";
+    } 
+public void StartGame()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
